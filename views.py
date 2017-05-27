1e673eadb4a70f8b1b7dd7cf907f4a1596ed6a87
kungsik@hotmail.com <kungsik@hotmail.com>
2017-05-27 22:24:29 +0300

    changed codes adding span classes

--- views.py
+++ views.py
@@ -6,26 +6,39 @@ from tf.fabric import Fabric
 ETCBC = 'hebrew/etcbc4c'
 TF = Fabric(modules=ETCBC)
 api = TF.load('''
-    sp lex voc_utf8
-    g_word trailer
-    qere qere_trailer
-    language freq_lex gloss
-    mother
+    book chapter verse
+    sp nu gn ps vt vs st
+    otype
+    det
+    g_word_utf8 trailer_utf8
+    lex_utf8 lex voc_utf8
+    g_prs_utf8 g_uvf_utf8
+    prs_gn prs_nu prs_ps g_cons_utf8
+    gloss 
 ''')
 api.makeAvailableIn(globals())
 
 @app.context_processor
 def utility_processor():
-    def view_text_line(book, chapter):
+    def view_text(book, chapter):
         chpNode = T.nodeFromSection((book, chapter))
-        verseNodes = L.d(chpNode, otype='verse')
-        verse_show = dict()
-        for v in verseNodes:
-            verseNum = T.sectionFromNode(v)[2]
-            verseTxt = T.text(L.d(v, otype='word'))
-            verse_show.update({verseNum:verseTxt})
-        return verse_show
-    return dict(view_text_line=view_text_line)
+        verseNode = L.d(chpNode, otype='verse')
+        verse = ""
+        for v in verseNode:
+            verse += '<span class=verse_num>'
+            verse += str(T.sectionFromNode(v)[2])
+            verse += '</span>'
+            wordsNode = L.d(v, otype='word')
+            for w in wordsNode:
+                verse += '<span class=word_elm>'
+                verse += F.g_word_utf8.v(w)
+                verse += '</span>'
+                if F.trailer_utf8.v(w):
+                    verse += '<span class=trailer>'
+                    verse += F.trailer_utf8.v(w)
+                    verse += '</span>'
+        return verse
+    return dict(view_text=view_text)
 
 
 @app.route('/')
