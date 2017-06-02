7c6bd9d26d8533eaf6e67fdc3e01045cb619ec8d
kungsik@hotmail.com <kungsik@hotmail.com>
2017-06-03 00:13:14 +0300

    fixed selector error

--- static/js/bible_selector.js
+++ static/js/bible_selector.js
@@ -1,8 +1,12 @@
 
-$(document).ready(function(){
+$(function(){
     var path = location.pathname.split('/');
     var book = path[2];
     var chapter = path[3];
+
+    //bible_selector id를 찾아 select 값 부여.
+    //$('#bible_selector[value='...']').attr("selected", "selected"); 과 동일한 효과 그런데 이 방식은 변수 인식이 어려움.
+    $('#bible_selector').val('/text/' + book);
 
     var chp = $("#bible_selector option:selected").attr("chp");
     for(var i=1; i <= chp; i++){
@@ -13,7 +17,5 @@ $(document).ready(function(){
             $("#chp_selector").append("<option value='/text/" + book + "/"+i+"'>"+i+"</option>");
         }
     }
-    //bible_selector id를 찾아 select 값 부여.
-    //$('#bible_selector[value='...']').attr("selected", "selected"); 과 동일한 효과 그런데 이 방식은 변수 인식이 어려움.
-    $('#bible_selector').val('/text/' + book);
+
 });
