b8cf836cc2da1bce83aa85e541d8ec921d382d1c
kungsik <kungsik@hotmail.com>
2018-03-20 22:58:33 +0200

    added the verbal form search option in word_api and corrected the link

--- static/js/conjugator.js
+++ static/js/conjugator.js
@@ -1,38 +1,57 @@
-$(function () {
-    $("#conjugator").submit(function () {
-        $("#load_image").show();
-        $("#conj_submit").hide();
+$(function() {
+  $("#conjugator").submit(function() {
+    $("#load_image").show();
+    $("#conj_submit").hide();
 
-        var conj_send = $.post('./', $('#conjugator').serialize())
-            .success(function () {
-                var data = $.parseJSON(conj_send.responseText)
-                if(data == false){
-                  var output = "<div class='alert alert-danger' role='alert' style='font-size:20px; text-align:center'>입력하신 단어가 동사인지 확인해 주세요.</div>";
-                }
-                else {
-                  var output = "<table class=table>"
-                  for (var i in data){
-                    output += "<tr><td align=right width=30%>" + data[i].verb + "</td>";
-                    output += "<td>" + data[i].lang + " " + data[i].stem + " " + data[i].tense + " " + data[i].ps + " " + data[i].gn + " " + data[i].nu;
-                    if(data[i]['prs_ps']) {
-                      output += " + " + data[i]['prs_ps'] + " " + data[i]['prs_gn'] + " " + data[i]['prs_nu'];
-                    }
-                    output += "</td><td width=30%>" + data[i].query + "</td></tr>"
-                  }
-                  output += "</table>"
-                }
+    var conj_send = $.post('./', $('#conjugator').serialize())
+      .success(function() {
+        var data = $.parseJSON(conj_send.responseText)
+        if (data == false) {
+          var output = "<div class='alert alert-danger' role='alert' style='font-size:20px; text-align:center'>입력하신 단어가 동사인지 확인해 주세요.</div>";
+        } else {
+          var output = "<table class=table>"
+          for (var i in data) {
+            output += "<tr><td align=right width=30%>" + data[i].verb + "</td>";
+            output += "<td>" + data[i].lang + " " + data[i].stem + " " + data[i].tense + " " + data[i].ps + " " + data[i].gn + " " + data[i].nu;
+            if (data[i]['prs_ps']) {
+              output += " + " + data[i]['prs_ps'] + " " + data[i]['prs_gn'] + " " + data[i]['prs_nu'];
+            }
+            output += "</td><td width=30%>" + data[i].query + "</td></tr>"
+          }
+          output += "</table>"
+        }
 
-                $("#load_image").hide();
-                $("#conj_submit").show();
+        $("#load_image").hide();
+        $("#conj_submit").show();
 
-                $("#verballist").html(output)
-            })
-            .error(function () {
-                alert("오류");
+        $("#verballist").html(output)
+      })
+      .error(function() {
+        alert("오류");
 
-                $("#load_image").hide();
-                $("#conj_submit").show();
-            });
-        return false;
-    });
+        $("#load_image").hide();
+        $("#conj_submit").show();
+      });
+    return false;
+  });
+});
+
+//get값으로 변수가 전달되었을 때 input에 그 값을 넣어 줌.
+$(function() {
+  $.urlParam = function(name) {
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+
+    if (!results) {
+      return '';
+    } else {
+      return results[1] || 0;
+    }
+  }
+
+  var cons = $.urlParam('cons');
+
+  if (cons) {
+    $('#verb').val(decodeURIComponent(cons));
+  }
+
 });
--- views.py
+++ views.py
@@ -63,7 +63,7 @@ book_abb = {
     "Ezra": "ezra",
     "Nehemiah": "neh",
     "1_Chronicles": "1chr",
-    "2_Chronicles": "2chr"    
+    "2_Chronicles": "2chr"
 }
 
 def word_function(node):
@@ -85,6 +85,10 @@ def word_function(node):
     w_f["성(접미)"] = F.prs_gn.v(node)  # pronominal suffix gender
     w_f["수(접미)"] = F.prs_nu.v(node)  # pronominal suffix number
     w_f["의미"] = F.gloss.v(L.u(node, otype='lex')[0])
+
+    if w_f["동사형"] != "NA" and w_f["동사형"] != "" and w_f["동사형"] != "unknown":
+        w_f["동사형태"] = "<a href='/conjugator/?cons=" + w_f["원형"] + "' target=_blank>검색</a>"
+
     return w_f
 
 @app.route('/')
