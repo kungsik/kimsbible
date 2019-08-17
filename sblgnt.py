a943e1af458ad1aa984f8b4e9a75aa1aa8647d57
kungsik@hotmail.com <kungsik@hotmail.com>
2019-08-17 19:27:18 +0900

    added map

--- sblgnt.py
+++ sblgnt.py
@@ -1,11 +1,43 @@
 from flask import render_template
 from kimsbible import app
 
+book_abb = {
+    "Matthew": "matt",
+    "Mark": "mark",
+    "Luke": "luke",
+    "John": "john",
+    "Acts": "acts",
+    "Romans": "rom",
+    "1_Corinthians": "1cor",
+    "2_Corinthians": "2cor",
+    "Galatians": "gal",
+    "Ephesians": "eph",
+    "Philippians": "phil",
+    "Colossians": "col",
+    "1_Thessalonians": "1thess",
+    "2_Thessalonians": "2thess",
+    "1_Timothy": "1tim",
+    "2_Timothy": "2tim",
+    "Titus": "titus",
+    "Philemon": "none",
+    "Hebrews": "heb",
+    "James": "none",
+    "1_Peter": "1pet",
+    "2_Peter": "2pet",
+    "1_John": "none",
+    "2_John": "none",
+    "3_John": "none",
+    "Jude": "jude",
+    "Revealation": "rev" 
+}
+
+
 @app.route('/sblgnt/')
 @app.route('/sblgnt/<book>')
 @app.route('/sblgnt/<book>/<int:chapter>')
 def sblgnt_page(book='Matthew', chapter=1):
-    return render_template('sblgnt_text.html', book=book, chapter=chapter)
+    kml_file = "http://alphalef.com/apps/kml/" + book_abb[book] + '.' + str(chapter) + '.' + "kml"
+    return render_template('sblgnt_text.html', book=book, chapter=chapter, kml_file=kml_file)
 
 @app.route('/sblgnt/word/<int:node>')
 def show_sblgnt_word_function(node):
