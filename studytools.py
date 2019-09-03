1bb0b5ddde91baae99fbcb17a2c98efa471e8c26
alphalef <kungsik@gmail.com>
2019-09-04 07:12:56 +0900

    working on the study tools

--- studytools.py
+++ studytools.py
@@ -13,22 +13,44 @@ api.makeAvailableIn(globals())
 def studytools():
     if request.method == 'POST':
         rangeCode = request.form['rangeCode']
-        nodeList = stat.codetorange(rangeCode)
-        rangeStr = stat.codetostr(rangeCode, stat.bookListKor)
-        sectionTitle = rangeStr.split("; ")
-        
-        if nodeList == False: return False
-        
-        result = ''
-        i = 0
+        sections = rangeCode.split(";")
 
-        # for title in sectionTitle:
-        #     result = "<h4>" + title + "</h4>"
-        for node in nodeList:
-            section = T.sectionFromNode(node)
-            chpvrs = str(section[1]) + ":" + str(section[2])
-            result += '<span class=chpvrs>' + chpvrs + '</span> <span class="verse">' + T.text(L.d(node, otype='word')) + '</span>'
-    
+        result = '<h3>본문읽기(구약 히브리어)</h3>'
+        vocalist = {}
+
+        for section in sections:
+            nodeList = stat.codetorange(section)
+            sectionTitle = stat.codetostr(section, stat.bookListKor)
+        
+            if nodeList == False: return False
+            
+            result += '<h4>' + sectionTitle + '</h4>'
+            result += '<br>'
+            result += '<div class="section" style="direction:rtl; text-align:right; column-count: 2;">'
+            
+            for node in nodeList:
+                section = T.sectionFromNode(node)
+                wordsNode = L.d(node, otype='word')
+                result += '<span class=chpvrs>' + str(section[2]) + '</span> <span class="verse">' + T.text(wordsNode) + '</span>'
+
+                for w in wordsNode:
+                    root = F.voc_utf8.v(L.u(w, otype='lex')[0])
+                    strong = get_strong(w)
+                    gloss = get_kor_hgloss(strong, w)
+
+                    if not root in vocalist:
+                        vocalist[root] = gloss
+            
+            result += '</div><br><br>'
+
+        result += '<h3>단어 리스트</h3>'
+        result += '<br>'
+        result += '<div style="column-count: 3;">'
+        sorted_vocalist = sorted(vocalist.items())
+        for voca in sorted_vocalist:
+            result += '<span>' + voca[0] + ' ' + voca[1] + '</span><br>'
+        
+        result += '</div>'
         return result
 
     else:
