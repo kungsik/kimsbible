6839f5f52c621734a9b7a9ddb263361d14c28627
alphalef <kungsik@gmail.com>
2019-09-11 09:47:17 +0900

    added robots.txt

--- __init__.py
+++ __init__.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, send_from_directory, request
 import os
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
+
+@app.route('/robots.txt')
+def robot_to_root():
+    return send_from_directory(app.static_folder, request.path[1:])
 
 
 import kimsbible.bhsheb
