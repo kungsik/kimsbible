97a5afec98902c70e5b12e007a9ffc966989785f
alphalef <kungsik@gmail.com>
2019-09-13 23:27:01 +0900

    working on enhanced view

--- bhsheb.py
+++ bhsheb.py
@@ -275,15 +275,15 @@ def text_page(book='Genesis', chapter=1):
                 chp_vrs = re.split(":", c_v)
                 kor_vrs = kb.json_to_verse(section[0], chp_vrs[0], chp_vrs[1], 'korean')
 
-            verse += "<p class='heb_korean' id='heb_korean' dir=ltr align=left>" + kor_vrs + "</p>"
+            verse += "<p class='heb_kor' id='heb_kor' dir=ltr align=left>" + kor_vrs + "</p>"
 
-            # #kjv 번역본
-            # eng_chp_vrs = kb.heb_vrs_to_eng(section[0], str(section[1]), str(section[2]))
-            # for c_v in eng_chp_vrs:
-            #     chp_vrs = re.split(":", c_v)
-            #     kjv_vrs = kb.json_to_verse(section[0], chp_vrs[0], chp_vrs[1], 'kjv')
+            #kjv 번역본
+            eng_chp_vrs = kb.heb_vrs_to_eng(section[0], str(section[1]), str(section[2]))
+            for c_v in eng_chp_vrs:
+                chp_vrs = re.split(":", c_v)
+                kjv_vrs = kb.json_to_verse(section[0], chp_vrs[0], chp_vrs[1], 'kjv')
 
-            # verse += "<p class='heb_korean' id='heb_kjv' dir=ltr align=left>" + kjv_vrs + "</p>"
+            verse += "<p class='heb_kjv' id='heb_kjv' dir=ltr align=left>" + kjv_vrs + "</p>"
 
             verse += '</li>'
 
