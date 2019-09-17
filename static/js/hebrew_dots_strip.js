364d925ecc283f7489866d3438c035dbd6451652
alphalef <kungsik@gmail.com>
2019-09-17 23:55:22 +0900

    added local storage data

--- static/js/hebrew_dots_strip.js
+++ static/js/hebrew_dots_strip.js
@@ -16,8 +16,39 @@ const doFix = () => {
     toReturn = optDagesh.checked ? removeDagesh(toReturn) : toReturn
     verse_container.innerHTML = toReturn
 
+    localStorage.accent = optAccents.checked ? 'on' : 'off'
+    localStorage.vowels = optVowels.checked ? 'on' : 'off'
+    localStorage.dagesh = optDagesh.checked ? 'on' : 'off'    
+
     $.getScript('/static/js/modal_word_api.js')
+
+    // 악센트 관련 체크시 번역본이 사라지는 오류 보정
+    if (localStorage.getItem(".kjv") == 'on' ) {
+        document.getElementById('kjv').style.display = "block"
+    }
+    if (localStorage.getItem(".kor") == 'on' ) {
+        document.getElementById('kor').style.display = "block"
+    } 
+
+    if (horizon == 'on') {
+        let currentText = document.location.href.split('/')[3];
+        showhorizon(currentText);
+    }
 }
+
 document.querySelector("#optAccents").addEventListener('change', doFix)
 document.querySelector("#optVowels").addEventListener('change', doFix)
 document.querySelector("#optDagesh").addEventListener('change', doFix)
+
+if (localStorage.accent == 'on') { 
+    document.getElementById("optAccents").checked = true 
+    doFix()
+}
+if (localStorage.vowels == 'on') { 
+    document.getElementById("optVowels").checked = true 
+    doFix()
+}
+if (localStorage.dagesh == 'on') { 
+    document.getElementById("optDagesh").checked = true 
+    doFix()
+}
--- static/js/text_tools.js
+++ static/js/text_tools.js
@@ -19,12 +19,14 @@ function initialize(kml) {
 	 if (document.getElementById(button_id).checked == true) {
 		for (var i = 0; i < myelements.length; i++) {
 			myelements[i].style.display = "block";
-		}
+    }
+    localStorage.setItem(transclassname, "on");
 	 }
 	 else {
 		for (var i = 0; i < myelements.length; i++) {
 			myelements[i].style.display = "none";
-		}
+    }
+    localStorage.setItem(transclassname, "off");
 	 }
  }
 
@@ -34,6 +36,9 @@ function initialize(kml) {
    let transversions = document.querySelectorAll('.transversions')
 
    if (document.getElementById('showhorizon').checked == true) {
+     
+     localStorage.setItem("horizon", "on");
+
      for (var i = 0; i < versenodes.length; i++) {
       versecontainers[i].style.display = "flex";
       versenodes[i].style.flex = "1";
@@ -52,6 +57,9 @@ function initialize(kml) {
      }
    }
    else {
+     
+     localStorage.setItem("horizon", "off");
+
      for (var i = 0; i < versenodes.length; i++) {
       versecontainers[i].style.display = "block";
       transversions[i].style.marginTop = "0px";
@@ -92,3 +100,42 @@ function initialize(kml) {
     }
     window.print();     
   }     
+
+let kor = localStorage.getItem(".kor");
+let kjv = localStorage.getItem(".kjv");
+let horizon = localStorage.getItem("horizon");
+let currentText = document.location.href.split('/')[3];
+
+if (horizon == 'on') {
+  document.getElementById('showhorizon').checked = true;
+  showhorizon(currentText);
+}
+else {
+  document.getElementById('showhorizon').checked = false;
+  showhorizon(currentText);
+}
+
+if (kor == 'on') {
+  document.getElementById('showkor').checked = true;
+  showTrans(".kor", 'showkor');
+}
+else {
+  document.getElementById('showkor').checked = false;
+  showTrans(".kor", 'showkor');
+}
+
+if (kjv == 'on') {
+  document.getElementById('showkjv').checked = true;
+  showTrans(".kjv", 'showkjv');
+}
+else {
+  document.getElementById('showkjv').checked = false;
+  showTrans(".kjv", 'showkjv');
+}
+
+// 초기 스토리지 세팅: 한글번역 보이기, 영어번역 숨기기
+if (!kjv && !kor) {
+  localStorage.setItem(".kor", "on")
+  document.getElementById('showkor').checked = true; 
+  showTrans(".kor", 'showkor');
+}
