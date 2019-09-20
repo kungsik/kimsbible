663223852125692a9fb85f78befb74c3da2bb722
alphalef <kungsik@gmail.com>
2019-09-20 12:48:15 +0900

    added disqus recent posts

---
+++ static/js/disqus_recent_posts.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+	$commentDiv = $("#disqus_recent");
+	
+	$.get("https://disqus.com/api/3.0/threads/listPosts.json?forum=alpaalrebseonggyeong&thread=7611588846&limit=6&api_key=8C1bWCyMgfsAEm9EDpCgboLD30M3sKHqN0izKBPJp2jgk0ceNxvAJjWsT3MH76k7", function(res, code) {
+		//Good response?
+		if(res.code === 0) {
+			var result = "";
+			for(var i=0, len=res.response.length; i<len; i++) {
+				var post = res.response[i];
+				console.dir(post);
+                var html = "<div class='author-box col-sm-3' style='margin: 10px'>";
+                html += "<div class='d-flex mb-4'>";
+                html += "<div class='mr-3'>" 
+                html += "<img src='" + post.author.avatar.small.permalink + "' alt='Image' class='img-fluid rounded-circle'>";
+                html += "</div>"
+                html += "<div class='mr-auto text-black'>"
+                html += "<a href='"+ post.author.profileUrl + "'>" + post.author.name + "</a>";
+                html += "</div></div>"
+				html += "<p><a href='/community'>" + post.raw_message.substr(0,150) + "...</a></p>";
+				html += "<p class='postRef'>Posted at " + post.createdAt.split("T")[0] + "</p>";
+				html += "</div>";
+				
+				result+=html;
+			}
+			$commentDiv.html(result);
+		}
+	});
+});
